05e4fb1670d30f990b8abdad44b889f83a914343
danshatter <isikakudaniel@yahoo.com>
2023-04-11 11:54:24 +0100

    added user_id to posts

--- graphql/types/Post.php
+++ graphql/types/Post.php
@@ -5,12 +5,11 @@ require __DIR__.'/../../vendor/autoload.php';
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 
-$userType = require __DIR__.'/User.php';
-
 return new ObjectType([
     'name' => 'Post',
     'fields' => [
         'id' => Type::nonNull(Type::id()),
+        'user_id' => Type::nonNull(Type::int()),
         'title' => Type::nonNull(Type::string()),
         'body' => Type::nonNull(Type::string()),
         'created_at' => Type::nonNull(Type::string()),
